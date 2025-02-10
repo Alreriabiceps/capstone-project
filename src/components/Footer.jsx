2b1e8a5986c34caa7fc84ca949d7ae14f2b0cc04
russelle roxas <russelleroxas11@gmail.com>
2025-02-11 01:38:16 +0800

    ok try

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer className="footer py-2 fixed-bottom">
       <div className="container">
         <div className="text-center mt-3">
-          <p>&copy; 2025 Roasdgddsggdg.</p>
+          <p>&copy; 2025 Robcom. All rights reserved.</p>
         </div>
       </div>
     </footer>
